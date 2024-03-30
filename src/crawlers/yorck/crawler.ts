3ceef10f512e089b4ef4bacacf1d11b85c93222b
Hanie Gharedaghi <h7.gharedaghi@gmail.com>
2024-03-30 19:24:31 +0100

    refactored yorck crawler and adds scraper

--- src/crawlers/yorck/crawler.ts
+++ src/crawlers/yorck/crawler.ts
@@ -1,17 +1,7 @@
 import { PlaywrightCrawler } from "crawlee";
+import { YorckScraper } from "./scraper.js";
 
 const LAYOUT_WRAPPER_SELECTOR = ".LayoutWrapper_children";
-
-const DATA_NODE_SELECTOR =
-  LAYOUT_WRAPPER_SELECTOR + " > .MuiGrid-root.MuiGrid-container > .MuiGrid-item";
-
-const SHOW_TIME_NODE_SELECTOR =
-  DATA_NODE_SELECTOR + " > :last-child > :last-child > .MuiBox-root > .MuiBox-root";
-
-const DESCRIPTION_NODE_SELECTOR = DATA_NODE_SELECTOR + " > :first-child";
-
-const METADATA_NODE_SELECTOR = DATA_NODE_SELECTOR + "> .MuiGrid-root.MuiGrid-container";
-
 const MOVIE_DETAIL_PAGE = "MOVIE_DETAIL_PAGE";
 const MOVIE_DETAIL_PAGE_LINK_SELECTOR = ".FilmListingItem a";
 
@@ -19,67 +9,9 @@ const crawler = new PlaywrightCrawler({
   async requestHandler({ request, page, enqueueLinks, log, pushData }) {
     if (request.label === MOVIE_DETAIL_PAGE) {
       await page.waitForSelector(LAYOUT_WRAPPER_SELECTOR);
+      const movie = await new YorckScraper(page).scrape();
 
-      //TITLE
-      const title = await page.locator("h2").first().textContent();
-
-      //DESCRIPTION
-      const description = await page.locator(DESCRIPTION_NODE_SELECTOR).first().textContent();
-
-      //METADATA
-      const metadata = {} as {
-        [key: string]: string;
-      };
-      const keys = await page.locator(METADATA_NODE_SELECTOR).first().locator("h5").allInnerTexts();
-      const values = await page
-        .locator(METADATA_NODE_SELECTOR)
-        .first()
-        .locator("span.MuiTypography-root")
-        .allInnerTexts();
-
-      keys.forEach((key, index) => {
-        metadata[key] = values[index];
-      });
-
-      //SHOWTIME
-      const showTimes = await Promise.all(
-        (
-          await page.locator(SHOW_TIME_NODE_SELECTOR).all()
-        ).map(async (locator) => {
-          const theater = await locator
-            .locator("nth=0")
-            .locator(".MuiTypography-root")
-            .first()
-            .textContent();
-
-          const showTimeAndVersions = await Promise.all(
-            (
-              await locator
-                .locator("nth=0")
-                .locator(".MuiBox-root")
-                .locator("nth=0")
-                .locator("nth=0")
-                .all()
-            ).map(async (locator) => ({
-              time: await locator
-                .locator(".MuiTypography-listingItem")
-                .locator("nth=0")
-                .textContent(),
-              version: await locator
-                .locator(".MuiTypography-listingItem")
-                .locator("nth=1")
-                .textContent(),
-            }))
-          );
-
-          return {
-            theater,
-            when: showTimeAndVersions,
-          };
-        })
-      );
-
-      pushData({ ...metadata, description, showTimes }, title ? title : "");
+      pushData(movie, movie.title);
     } else {
       await page.waitForSelector(MOVIE_DETAIL_PAGE_LINK_SELECTOR);
       await enqueueLinks({
---
+++ src/crawlers/yorck/scraper.ts
@@ -0,0 +1,97 @@
+import { Page } from "playwright";
+import { dictionary, showTime, movie } from "../movie.js";
+
+const LAYOUT_WRAPPER_SELECTOR = ".LayoutWrapper_children";
+
+const DATA_NODE_SELECTOR =
+  LAYOUT_WRAPPER_SELECTOR + " > .MuiGrid-root.MuiGrid-container > .MuiGrid-item";
+
+const DESCRIPTION_NODE_SELECTOR = DATA_NODE_SELECTOR + " > :first-child";
+const METADATA_NODE_SELECTOR = DATA_NODE_SELECTOR + "> .MuiGrid-root.MuiGrid-container";
+const SHOW_TIME_NODE_SELECTOR =
+  DATA_NODE_SELECTOR + " > :last-child > :last-child > .MuiBox-root > .MuiBox-root";
+
+export class YorckScraper {
+  private page: Page;
+  constructor(page: Page) {
+    this.page = page;
+  }
+  public async scrape(): Promise<movie> {
+    const metadata = await this.getMetadata();
+    return {
+      title: await this.getTitle(),
+      description: await this.getDescription(),
+      cast: metadata["Cast"],
+      director: metadata["Director"],
+      showTimes: await this.getShowTimes(),
+    };
+  }
+
+  private async getTitle(): Promise<string> {
+    const title = await this.page.locator("h2").first().textContent();
+    return title ? title : "";
+  }
+
+  private async getDescription(): Promise<string> {
+    const description = await this.page.locator(DESCRIPTION_NODE_SELECTOR).first().textContent();
+    return description ? description : "";
+  }
+
+  private async getMetadata(): Promise<dictionary> {
+    const metadata: dictionary = {};
+    const keys = await this.page
+      .locator(METADATA_NODE_SELECTOR)
+      .first()
+      .locator("h5")
+      .allInnerTexts();
+    const values = await this.page
+      .locator(METADATA_NODE_SELECTOR)
+      .first()
+      .locator("span.MuiTypography-root")
+      .allInnerTexts();
+
+    keys.forEach((key, index) => {
+      metadata[key] = values[index];
+    });
+    return metadata;
+  }
+
+  private async getShowTimes(): Promise<showTime[]> {
+    return (
+      await Promise.all(
+        (
+          await this.page.locator(SHOW_TIME_NODE_SELECTOR).all()
+        ).map(async (locator) => {
+          const theater =
+            (await locator.locator("nth=0").locator(".MuiTypography-root").first().textContent()) ||
+            "";
+
+          return await Promise.all(
+            (
+              await locator
+                .locator("nth=0")
+                .locator(".MuiBox-root")
+                .locator("nth=0")
+                .locator("nth=0")
+                .all()
+            ).map(async (locator) => ({
+              theater,
+              screenType: "2D",
+              time:
+                (await locator
+                  .locator(".MuiTypography-listingItem")
+                  .locator("nth=0")
+                  .textContent()) || "",
+              versions: [
+                (await locator
+                  .locator(".MuiTypography-listingItem")
+                  .locator("nth=1")
+                  .textContent()) || "",
+              ],
+            }))
+          );
+        })
+      )
+    ).flat();
+  }
+}
